ca69ed1915a95e60c35fa19f6b0223f575917456
crazypon <ilnuryamaletdinov349@gmail.com>
2025-07-16 16:16:36 +0500

    Collections Tutorial added

---
+++ src/CollectionsFramework/PriorityQueueTutorial.java
@@ -0,0 +1,10 @@
+package CollectionsFramework;
+
+public class PriorityQueue {
+    /*
+    This is heap-like data structure, it guarantees that elements can be removed in priority order
+     */
+    public static void main(String[] args) {
+        PriorityQueue pq = new Pri
+    }
+}
